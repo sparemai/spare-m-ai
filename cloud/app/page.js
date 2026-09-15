5a6fb508ab504b3753aefa7a44b9895ca7a6d667
sparemai <vinthasreelakshmi@gmail.com>
2026-09-15 09:37:24 +0530

    Use unified SPARE-M cockpit as the main page

--- cloud/app/page.js
+++ cloud/app/page.js
@@ -1,3 +1,2 @@
 import Dashboard from './components/Dashboard';
-import CoveragePanel from './components/CoveragePanel';
-export default function Page(){return <><Dashboard/><CoveragePanel/></>}
+export default function Page(){return <Dashboard/>}
